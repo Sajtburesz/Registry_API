1d69c285d9deb49ec927efb18a300f9362bbe661
Bihácsi Bence <bihacsi.bence@gmail.com>
2024-07-18 16:54:48 +0200

    added check and started diff

--- index.js
+++ index.js
@@ -33,12 +33,15 @@ class Registry {
     invertRegistry(){
         this.inverted = !this.inverted;
     }
+
+    checkItem(item){
+        return this.inverted + this.registry.has(item);
+    }
 }
 
 
 
 const registry = new Registry();
-
 
 
 app.post("/add", (req, res, next) => {
@@ -73,6 +76,23 @@ app.post("/invert", (req, res, next) => {
 
 });
 
+app.get("/check/:item", (req, res, next) => {
+    const item = req.params.item;
+
+    if (typeof item === "string" && isAlphanumeric(item)){
+        if ( registry.checkItem(item) == 1){
+            res.status(200).send( item + ' is in the registry.');
+        } else {
+            res.status(400).send( item + ' is not in the registry.');
+        }
+    } else {
+        res.status(422).send('Item contains non alphanumeric characters or item is not a string.');
+    }
+});
+
+app.post("/diff", (req, res, next) => {
+    const { items }  = req.body;
+});
 
 
 // Function for check if string is alphanumeric using regex.
