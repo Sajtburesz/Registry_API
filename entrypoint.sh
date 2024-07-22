9acefdceaaffb34080e7e6d12043094dcd2f28a1
Sajtburesz <93286464+Sajtburesz@users.noreply.github.com>
2024-07-22 14:11:31 +0200

    changed amount of application server workers from 2 to 1 (#3)

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,5 @@ if [ "$TEST" = "true" ]; then
     python3 -m unittest test_registry.py
 else
     echo "Starting application..."
-    exec gunicorn -w 2 -b 0.0.0.0:8000 registry:app
+    exec gunicorn -w 1 -b 0.0.0.0:8000 registry:app
 fi
